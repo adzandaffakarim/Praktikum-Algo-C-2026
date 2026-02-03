766c75b46d0092355c57429fb8cbde45386a4d78
adzandaffakarim <adzandaffa1416Gmail.com>
2026-02-03 21:24:43 +0700

    biodata.py

---
+++ Adzan_Daffa_Karim_25071105146_TI-C/Latihan_Praktikum/biodata.py
@@ -0,0 +1,10 @@
+nama = 'Adzan Daffa Karim'
+kls = 'TI-C'
+nim= 25071105146
+uni = 'UNIVERSITAS RIAU'
+fac = 'Teknik'
+
+print ('Halo, namaku ' + nama, 'aku dari kelas', kls)
+print ('dengan NIM ', str(nim))
+print ('Aku kuliah di ' + uni)
+print ('Fakultas ' + fac)
